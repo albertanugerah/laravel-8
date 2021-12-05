b498fcbc851d63b728848eb8cbb35e7181460135
Albert Anugerah Putra Siregar <albertanugerah@hotmail.com>
2021-12-06 00:01:02 +0700

    create middleware

---
+++ laravel/app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request  $request
+     * @param  Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->role || $request->role != 'admin') {
+            return redirect('/');
+        }
+        return $next($request);
+    }
+}
